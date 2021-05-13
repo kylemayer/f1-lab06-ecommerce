4fd90745b0bd7c5efc669d4b3693f250c1515682
kmayer <kylemayer@x.local>
2021-05-13 10:05:45 -0700

    update

--- cart/cart.js
+++ cart/cart.js
@@ -25,10 +25,10 @@ total.textContent = `Total: ${totalPrice.toLocaleString ('en-US', {
 const placeButton = document.getElementById('place');
 // const emptyButton = document.getElementById('empty');
 
-placeButton.addEventListener('click', function () {
+placeButton.addEventListener('click', function (){
     alert(`You're order has been placed! Please wait 17 weeks before arrival.`);
 });
 
-// emptyButton.addEventListener('click', function () {
+// emptyButton.addEventListener('click', function (){
 
 // });
